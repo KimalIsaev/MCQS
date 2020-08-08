242055f5aa3d98e7a0dd77b3c11721e82eb3d886
ValeriyaRyzhikova <rvaleriav@yandex.ru>
2020-08-09 02:37:30 +0700

    Добавлены Фунции изменения векторов фаз и орбиты,перегружен оператор сложения в классе state_change

--- state_change.cpp
+++ state_change.cpp
@@ -1,6 +1,6 @@
 #include "state_change.h"
 
-StateChange::StateChange(const std::vector<int> orbit, const std::vector<int> phase)
+StateChange::StateChange(const std::span<int> orbit, const std::span<int> phase)
 {
 	orbit_change_ = orbit;
 	phase_change_ = phase;
@@ -11,19 +11,7 @@ StateChange::StateChange(const StateChange& orig)
 	orbit_change_ = orig.orbit_change_;
 	phase_change_ = orig.phase_change_;
 }
-/* убрано так как хочется исключить возможности изменения в данном классе 
-bool StateChange::setStateChange(vector<int> orbit, vector<int> phase)
-{
-	if (!existence)
-	{
-		orbitChange = orbit;
-		phaseChange = phase;
-		existence = true;
-		return true;
-	}
-	return false;
-}
-*/
+
 
 StateChange& StateChange::operator=(const StateChange &newStateChange)
 {
@@ -32,16 +20,49 @@ StateChange& StateChange::operator=(const StateChange &newStateChange)
 	return *this;
 }
 
-//эта функции должна возвращать StateChange с orbit_change_ и пустым phase_change_ 
-StateChange StateChange::StateChangeWithOrbit(const std::vector<int>)
+StateChange& StateChange::operator=(const StateChange& newStateChange)
 {
-	return StateChange();
+	int thisSize = this->phase_change_.size();
+	int newSize = newStateChange.phase_change_.size();
+	StateChange resultStateChange;
+	if (!thisSize || !newSize)
+		if (!thisSize)
+			resultStateChange.phase_change_ = newStateChange.phase_change_;
+		else
+			resultStateChange.phase_change_ = this->phase_change_;
+	else
+	{
+		int i;
+		for (i = 0; i < thisSize && i < newSize; i++)
+			resultStateChange.phase_change_[i] = resultStateChange.phase_change_[i] + this->phase_change_[i];
+		for (; i < thisSize; i++)
+			resultStateChange.phase_change_[i] =this->phase_change_[i];
+		for (; i < newSize; i++)
+			resultStateChange.phase_change_[i] = newStateChange.phase_change_[i];
+	}
+
+	thisSize = this->orbit_change_.size();
+	newSize = newStateChange.orbit_change_.size();
+	StateChange resultStateChange;
+	if (!thisSize || !newSize)
+		if (!thisSize)
+			resultStateChange.orbit_change_ = newStateChange.orbit_change_;
+		else
+			resultStateChange.orbit_change_ = this->orbit_change_;
+	else
+	{
+		int i;
+		for (i = 0; i < thisSize && i < newSize; i++)
+			resultStateChange.orbit_change_[i] = resultStateChange.orbit_change_[i] + this->orbit_change_[i];
+		for (; i < thisSize; i++)
+			resultStateChange.orbit_change_[i] = this->orbit_change_[i];
+		for (; i < newSize; i++)
+			resultStateChange.orbit_change_[i] = newStateChange.orbit_change_[i];
+	}
+
+	return resultStateChange;
 }
 
-StateChange StateChange::StateChangeWithPhase(const std::vector<int>)
-{
-	return StateChange();
-}
 
 std::ostream& operator<<(std::ostream& os, const StateChange x) 
 {
@@ -53,3 +74,16 @@ std::ostream& operator<<(std::ostream& os, const StateChange x)
 	return os;
 }
 
+StateChange StateChange::StateChangeWithOrbit(const std::span<int> new_orbit)
+{
+	StateChange a;
+	a.orbit_change_ = new_orbit;
+	return a;
+}
+
+StateChange StateChange::StateChangeWithPhase(const std::span<int> new_phase)
+{
+	StateChange a;
+	a.orbit_change_ = new_phase;
+	return a;
+}
